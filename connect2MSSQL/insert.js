24286dbcde6cd9fb1b21bde6b7b4fc06f86a7971
imneverdied <imneverdied@gmail.com>
2021-05-01 00:54:41 +0800

    Create insert.js

---
+++ connect2MSSQL/insert.js
@@ -0,0 +1,38 @@
+
+var express = require('express');
+var app = express();
+app.get('/', function (req, res) {
+    var sql = require('mssql');
+
+    //config for your database
+    var config = {
+        user: '...',          // your username
+        password: '...',      // your password
+        server: 'localhost',  // You can use 'localhost\\instance' to connect to named instance
+        database: '...',      // your database
+        port: 1433            // Microsoft Management Console
+
+    };
+
+    //connect to your database
+    sql.connect(config, function (err) {
+        if (err) console.log(err);
+
+        //create Request object
+        var request = new sql.Request();
+
+        request.query("INSERT INTO NODE VALUES ('DATA1','DATA2');", function (err, data) {
+            if (err) { console.log(err); }
+            else {
+                console.log('success');
+                res.send('success');
+            }
+
+        });
+    });
+
+});
+
+var server = app.listen(8081, function () {
+    console.log('Server is running!');
+});
